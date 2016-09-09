17cf5b39fcff99166256b9128ce098db1afac952
Derek <ccumusical@gmail.com>
2016-09-09 10:21:33 +0800

    fix

--- src/mentionSuggestionsStrategy.js
+++ src/mentionSuggestionsStrategy.js
@@ -4,5 +4,5 @@ import findWithRegex from 'find-with-regex';
 import escapeRegExp from 'lodash/escapeRegExp';
 
 export default (trigger: String) => (contentBlock: Object, callback: Function) => {
-  findWithRegex(new RegExp(`(\\s|^)${escapeRegExp(trigger)}[\\w]*`, 'g'), contentBlock, callback);
+  findWithRegex(new RegExp(`(\\s|^)${escapeRegExp(trigger)}[\u4e00-\u9fa5_a-zA-Z0-9]*`, 'g'), contentBlock, callback);
 };
